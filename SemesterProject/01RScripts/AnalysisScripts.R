248625e67853cb38d4db64e7e6a186e3f9db136a
amerkl <amerkl1@lsu.edu>
2019-04-20 20:33:56 -0500

    Code Updates

--- SemesterProject/01RScripts/AnalysisScripts.R
+++ SemesterProject/01RScripts/AnalysisScripts.R
@@ -5,22 +5,30 @@ library(ggplot2)
 ggplot(data=subset(JoinedData, !is.na(LSUPoints)), mapping = aes(x=Opponent, fill=Outcome)) +
   geom_bar()+
   theme(axis.text.x = element_text(angle = 45))+
-  ggtitle("Count of Crimes by LSU Opponents")
+  ggtitle("Count of Crimes by LSU Opponents")+
+  xlab("Opponent Universities")+
+  ylab("Total Crime")
 #Crime Count by Opponents
 
 ggplot(data=subset(JoinedData, !is.na(LSUPoints)), mapping = aes(x=Outcome, fill=crime)) +
   geom_bar()+
-  ggtitle("TotalCrime by LSU Outcome")
+  ggtitle("TotalCrime by LSU Outcome")+
+  xlab("Crime Type")+
+  ylab("Total Crime")
 
 ggplot(data=subset(JoinedData, !is.na(Outcome)), mapping = aes(x=crime, fill=Outcome)) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90))+
-  ggtitle("TotalCrime by LSU Outcome")
+  ggtitle("TotalCrime by LSU Outcome")+
+  xlab("Crime Type")+
+  ylab("Total Crime")
 #Crime by Outcome
 
 ggplot(data = LSUFootball) +
   geom_point(mapping = aes(x = GameAttendance, y = LSUPoints, color = Outcome, size = 9)) +
-  ggtitle("LSU Points Scored by Game Attendance", "Colored by Outcome")
+  ggtitle("LSU Points Scored by Game Attendance", "Colored by Outcome")+
+  xlab("Attendance")+
+  ylab("LSU Points Scored")
 #Point by Game Attendance Plot
 
 ggplot(data=subset(JoinedData, !is.na(district)), mapping = aes(x=crime, fill=district)) +
@@ -34,11 +42,16 @@ ggplot(data=subset(JoinedData, !is.na(district)), mapping = aes(x=crime, fill=di
 ggplot(data=subset(JoinedData, !is.na(a_c))) +
   geom_bar(mapping = aes(x = crime, fill = a_c)) +
   ggtitle("Total Crimes for 2018", "Separated by Committed and Attempted") +
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90))+
+  xlab("Crime Type")+
+  ylab("Total Crime")
 #Crimes by Committed vs Attempted
 
 ggplot(data=JoinedData, mapping = aes(x=district, fill = crime)) +
-  geom_bar()
+  geom_bar()+
+  ggtitle("Crime Count by District","Filtered By Crime Type")+
+  xlab("District Number")+
+  ylab("Crime Count")
 #Crime by District
 
 ggplot(data=subset(JoinedData, !is.na(Outcome)), mapping = aes(x=offense_date))+
