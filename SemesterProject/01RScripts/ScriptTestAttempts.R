4e2db5ee77ac4bf47849efa144f733f4b834a4da
amerkl <amerkl1@lsu.edu>
2019-04-20 20:12:58 -0500

    README and analysis scripts updates

--- SemesterProject/01RScripts/ScriptTestAttempts.R
+++ SemesterProject/01RScripts/ScriptTestAttempts.R
@@ -1,6 +1,4 @@
-library(plyr)
-library(ggplot2)
-
+#INCOMPLETE SCRIPTS
 
 
 #average crime count per lsu date per non lsu date INCOMPLETE
@@ -30,12 +28,6 @@ JoinedData  %>%
   count(crime)->CrimeTypeDF
 ggplot(data = CrimeTypeDF, mapping = aes(crime))+
   geom_bar()
-  
-#Count of Crimes by Opponent
-ggplot(data=subset(JoinedData, !is.na(LSUPoints)), mapping = aes(x=Opponent, fill=Outcome)) +
-  geom_bar()+
-  theme(axis.text.x = element_text(angle = 45))+
-  ggtitle("Count of Crimes by LSU Opponents")
 
 max(JoinedData$crime)
 
@@ -45,29 +37,8 @@ as.data.frame(JoinedData$crime)->cdf
 
 JoinedData$offense_date +
   filter(str_detect(offense_date, "2018-09-02"|"2018-09-08"|"2018-09-15"|"2018-09-22"|"2018-09-29"|"2018-10-06"|"2018-10-13"|
-         "2018-10-20"|"2018-11-03"|"2018-11-10"|"2018-11-17"|"2018-11-24"))
+                      "2018-10-20"|"2018-11-03"|"2018-11-10"|"2018-11-17"|"2018-11-24"))
 
-z#count_(JoinedData$Outcome, sort = true)
-
-#JoinedData$LSUPoints>=AvgPoints 
-
-#ggplot(data = JoinedData) +
-  #geom_bar(mapping = aes(x = crime, color = CheckDates))  
-
-#geom_smooth(mapping = aes(x = GameAttendance, y = LSUPoints))
-
-#JoinedData$color <- ifelse(JoinedData$Outcome == "NA" & JoinedData$LSUPoints <= AvgPoints, "orange", NA)
-
-
-#crime count per LSU outcome (COMPLETED)
-ggplot(data=subset(JoinedData, !is.na(LSUPoints)), mapping = aes(x=Outcome, fill=crime)) +
-  geom_bar()+
-  ggtitle("TotalCrime by LSU Outcome")
-
-ggplot(data=subset(JoinedData, !is.na(Outcome)), mapping = aes(x=crime, fill=Outcome)) +
-  geom_bar() +
-  theme(axis.text.x = element_text(angle = 90))+
-  ggtitle("TotalCrime by LSU Outcome")
 
 #Crime per audience count (if audience is above average) INCOMPLETE
 ggplot(data=subset(JoinedData, is(Outcome)), mapping = aes(x=crime, fill=Outcome)) +
@@ -78,9 +49,6 @@ ggplot(data=subset(JoinedData, is(Outcome)), mapping = aes(x=crime, fill=Outcome
 #Crime for LSU Point Parameters (COMPLETED)
 mean(LSUFootball$LSUPoints)->AvgPoints
 
-ggplot(data = LSUFootball) +
-  geom_point(mapping = aes(x = GameAttendance, y = LSUPoints, color = Outcome, size = 9)) +
-  ggtitle("LSU Points Scored by Game Attendance", "Colored by Outcome")
 
 filter_(JoinedData$Outcome, "NA")
 
@@ -90,29 +58,13 @@ ggplot(data=subset(JoinedData, !is.na(Outcome)), mapping = aes(x=crime, fill=Out
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90)) -> co
 
-  co+facet_grid(vars(Outcome))
+co+facet_grid(vars(Outcome))
 
-
-#Total 2018 Crime by Crime Type (potential add on for location)
-ggplot(data=subset(JoinedData, !is.na(district)), mapping = aes(x=crime, fill=district)) +
-  geom_bar() +
-  theme(axis.text.x = element_text(angle = 90)) +
-  ggtitle("Total Crime by Type per District","Colored by Outcome")+
-  xlab("Total Crime")+
-  ylab("Type of Crime")
 
 
 #Total 2018 Crime by District
 ggplot(data=JoinedData, mapping = aes(x=district, fill = crime)) +
   geom_bar() 
-
-#Count of Crime by crime type in 2018
-ggplot(data=subset(JoinedData, !is.na(a_c))) +
-  geom_bar(mapping = aes(x = crime, fill = a_c)) +
-  ggtitle("Total Crimes for 2018", "Separated by Committed and Attempted") +
-  theme(axis.text.x = element_text(angle = 90))
-
-
 
 #trying stuff
 ggplot(data = LSUFootball) +
@@ -120,7 +72,7 @@ ggplot(data = LSUFootball) +
 
 ggplot(data = LSUFootball) +
   geom_point(mapping = aes(x = GameAttendance, y = LSUPoints, color = Outcome, size = 9))  #) #+
-  #geom_smooth(mapping = aes(x = GameAttendance, y = LSUPoints))
+#geom_smooth(mapping = aes(x = GameAttendance, y = LSUPoints))
 #Graphic showing how Game Attendance Affects the Outcome and Points Scored over 2018
 
 ggplot(data=JoinedData) +
@@ -147,10 +99,5 @@ length(crimeVector)
 c(JoinedData$crime)->crimeVector
 count(JoinedData$crime)  
 
-ggplot(data=subset(JoinedData, !is.na(Outcome)), mapping = aes(x=offense_date))+
-  geom_bar() ->p
-
-p+facet_grid(vars(crime))+
-  ggtitle("Crime Count per District during Football Season")
 
 CrimeTypeDF$crime->CrimeTypes
--- SemesterProject/README.md
+++ SemesterProject/README.md
@@ -1,1 +1,8 @@
-# 3070Project
+#Research Question:
+Do LSU events influence Crime Rates in the Baton Rouge area?
+
+##The Data
+The data used for the analysis comes from Open Data BR (https://data.brla.gov/Public-Safety/Baton-Rouge-Crime-Incidents/fabb-cnnu) Crime Safety API and the 2018 LSU Football Schedule. These two sources were joined together to analyze how the different factors of LSU Football games, such as Attendance, Game Outcome, Opponent, and Points Scored, can affect crime rates overall as well as specific crime type rates, to see where safety might be lacking.
+
+#Conclusion
+LSU events do not have a significant impact on the amount of crimes committed a day, but rather the types of crimes that are committed. The details of the analysis are covered in the HTML and PDF analysis reports.
